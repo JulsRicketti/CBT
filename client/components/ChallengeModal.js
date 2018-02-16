330abeea4e42e4d967e26268cbc64d180d95f3fa
Julia Ricketti <julsricketti@gmail.com>
2018-02-16 13:29:40 -0200

    Separate the ChallengeModal into its own component

---
+++ client/components/ChallengeModal.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import Page from '../components/Page'
+import { Form, Segment, Tab, Button, Icon, Modal, Divider, Label, Grid } from 'semantic-ui-react'
+
+export default class ChallengeModal extends React.Component {
+
+  constructor (props) {
+    super(props)
+    
+    this.state = {
+      showNewChallengeForm: false,
+      title: '',
+      description: '',
+      difficulty: 0
+    }
+  }
+
+  disableCreateButton () {
+    const { title, description, difficulty } = this.state
+    return !title || !description || !difficulty
+  }
+
+  handleChangeForms (target, value) {
+    const obj = {}
+    obj[target] = value
+    this.setState(obj)
+  }
+
+  handleCreateButton (evt) {
+    evt.preventDefault()
+    
+    const { title, description, difficulty } = this.state
+    const { setTitle, setDescription, setDifficulty } = this.props
+
+    setTitle(title)
+    setDescription(description)
+    setDifficulty(difficulty)
+    this.closeModal()
+  }
+
+  closeModal () {
+    this.setState({ showNewChallengeForm: false })
+  }
+
+  showModal () {
+    this.setState({showNewChallengeForm: true})
+    $(this.modal).modal('show')
+  }
+
+  render() {
+    const {
+      title,
+      description,
+      difficulty,
+      showNewChallengeForm
+    } = this.state
+
+    return (
+      <Modal closeIcon onClose={() => this.closeModal()} open={showNewChallengeForm} trigger={<Button onClick={() => this.setState({showNewChallengeForm: true})}><Icon className='plus'/>New Challenge</Button>}>
+        <Modal.Header>New Challenge</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Input
+              label='Challenge title'
+              value={title}
+              onChange={(evt) => this.handleChangeForms('title', evt.target.value)}
+            />
+            <Form.TextArea
+              label='Challenge description'
+              value={description}
+              onChange={(evt) => this.handleChangeForms('description', evt.target.value)}
+            />
+            <Form.Input
+              label='Difficulty'
+              type='number'
+              min={1}
+              max={10}
+              placeholder='Select difficulty'
+              value={difficulty}
+              onChange={(evt) => this.handleChangeForms('difficulty', evt.target.value)}
+            />
+            <Button onClick={(evt) => this.handleCreateButton(evt)} disabled={this.disableCreateButton()}>Create</Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
--- client/pages/challenges.js
+++ client/pages/challenges.js
@@ -4,16 +4,13 @@ import { Form, Segment, Tab, Button, Icon, Modal, Divider, Label, Grid } from 's
 import axios from 'axios'
 import withRedux from 'next-redux-wrapper'
 import { createStore, actions } from '../store'
+import ChallengeModal from '../components/ChallengeModal'
 class Challenges extends React.Component {
 
   constructor (props) {
     super(props)
 
     this.state = {
-      showNewChallengeForm: false,
-      title: '',
-      description: '',
-      difficulty: 0,
       challenges: []
     }
   }
@@ -25,73 +22,6 @@ class Challenges extends React.Component {
         console.warn('hereeee', res)
         this.setState({ challenges: res.data })
       })
-  }
-
-
-  disableCreateButton () {
-    const { title, description, difficulty } = this.state
-    return !title || !description || !difficulty
-  }
-
-  handleChangeForms (target, value) {
-    const obj = {}
-    obj[target] = value
-    this.setState(obj)
-  }
-
-  handleCreateButton (evt) {
-    evt.preventDefault()
-    
-    const { title, description, difficulty } = this.state
-    const { setTitle, setDescription, setDifficulty } = this.props
-
-    setTitle(title)
-    setDescription(description)
-    setDifficulty(difficulty)
-    this.closeModal()
-  }
-
-  closeModal () {
-    this.setState({ showNewChallengeForm: false })
-  }
-
-  renderModalForm () {
-    const {
-      title,
-      description,
-      difficulty,
-      showNewChallengeForm
-    } = this.state
-
-    return (
-      <Modal closeIcon onClose={() => this.closeModal()} open={showNewChallengeForm} trigger={<Button onClick={() => this.setState({showNewChallengeForm: true})}><Icon className='plus'/>New Challenge</Button>}>
-        <Modal.Header>New Challenge</Modal.Header>
-        <Modal.Content>
-          <Form>
-            <Form.Input
-              label='Challenge title'
-              value={title}
-              onChange={(evt) => this.handleChangeForms('title', evt.target.value)}
-            />
-            <Form.TextArea
-              label='Challenge description'
-              value={description}
-              onChange={(evt) => this.handleChangeForms('description', evt.target.value)}
-            />
-            <Form.Input
-              label='Difficulty'
-              type='number'
-              min={1}
-              max={10}
-              placeholder='Select difficulty'
-              value={difficulty}
-              onChange={(evt) => this.handleChangeForms('difficulty', evt.target.value)}
-            />
-            <Button onClick={(evt) => this.handleCreateButton(evt)} disabled={this.disableCreateButton()}>Create</Button>
-          </Form>
-        </Modal.Content>
-      </Modal>
-    )
   }
 
   renderChallenges (status) {
@@ -123,12 +53,6 @@ class Challenges extends React.Component {
     )
   }
 
-
-  showModal () {
-    this.setState({showNewChallengeForm: true})
-    $(this.modal).modal('show')
-  }
-
   render () {
     const { pathname } = this.props.url
     const { showNewChallengeForm } = this.state
@@ -144,7 +68,7 @@ class Challenges extends React.Component {
           come in direct contact with their fears, register and observe how they felt,
           how they reacted and the results of their challenges.
         </h4>
-        {this.renderModalForm()}
+        <ChallengeModal/>
         <Divider/>
         <Tab panes={panes}/>
       </Page>
