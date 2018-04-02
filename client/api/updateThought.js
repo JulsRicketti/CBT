edfac603d71923bd3ea421c927a5e966c207a43b
Julia Ricketti <julsricketti@gmail.com>
2018-04-02 14:43:17 -0400

    Initial setting up of the thought CRUD operations

--- client/api/updateThought.js
+++ client/api/updateThought.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 import Config from '../config/config'
 
-export default function updateChallenge (userId, accessToken, thoughtId, date, where, what, expectation, reality, strengthInitial, automaticThought, supportingEvidence, nonSupportingEvidence, thinkingErrors, newBelief, strengthFinal) {
+export default function updateChallenge (userId, accessToken, thoughtId, attributes) {
+  console.warn('thoughtId', thoughtId)
+  if(attributes.thinkingErrors) {
+    attributes.thinkingErrors = attributes.thinkingErrors.toString()
+    console.warn('attributes.thinkingErrors', attributes.thinkingErrors)
+  }
   return axios.patch(`${Config.serverUrl}/thoughts/${thoughtId}`,
-    { date, where, what, expectation, reality, strengthInitial, automaticThought, supportingEvidence, nonSupportingEvidence, thinkingErrors, newBelief, strengthFinal, userId },
+    attributes,
     { params: { access_token: accessToken } })
   .then((updatedChallenge)=> updatedChallenge.data)
   .catch(err => console.log(err))
