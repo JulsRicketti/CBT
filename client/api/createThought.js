33214a38557bf494e0e8bfdc820fb39e03881ebe
Julia Ricketti <julsricketti@gmail.com>
2018-04-21 09:52:19 -0400

    Fix the createThought  api function to set the userId property

--- client/api/createThought.js
+++ client/api/createThought.js
@@ -3,7 +3,7 @@ import Config from '../config/config'
 
 export default function createThought (userId, accessToken, date, where, what, expectation, reality) {
   return axios.post(`${Config.serverUrl}/thoughts`, {
-    date, where, what, expectation, reality},
+    date, where, what, expectation, reality, userId},
     {params: { access_token: accessToken }
   })
   .then((newThought)=> newThought.data)
