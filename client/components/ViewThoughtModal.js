bd93013a073445154cec3d1e9714841d7367069b
Julia Ricketti <julsricketti@gmail.com>
2018-05-20 11:38:41 -0400

    Fix View modal

--- client/components/ViewThoughtModal.js
+++ client/components/ViewThoughtModal.js
@@ -22,65 +22,76 @@ export default class ViewThoughtModal extends React.Component {
     this.setState({ showViewThoughtModal: false })
   }
 
-  displayField (field) {
+  handlePrint () {
+    console.warn('TODO')
+  }
 
-    if (Array.isArray(field)) {
-      console.warn('hello!', field)
-    }
-    // TODO: when something hasn't been completed yet, link to the new thought page to complete it!
+  displayField (field) {
     return field ? field : <span>Not yet completed</span>
+  }
+
+  displayThinkingErrors (thinkingErrors) {
+    const thinkingErrorsArray = thinkingErrors.split(',')
+
+    return thinkingErrorsArray.map(thinkingError =>
+        <li>{thinkingError}</li>
+      )
   }
 
   render() {
     const { thought } = this.props
     const modalTrigger = <Button onClick={() => this.setState({showViewThoughtModal: true})}>View</Button>
-    console.warn(thought)
+
     return (
       <Modal closeIcon onClose={() => this.closeModal()} trigger={modalTrigger}>
-        <Modal.Header>View Thought</Modal.Header>
+        <Modal.Header>
+          View Thought
+          <Divider/>
+          <Button onClick={() => this.handlePrint()}><Icon name='print'/> Print</Button>
+        </Modal.Header>
 
-        <Modal.Content>
-          <Button><Icon name='print'/> Print</Button>
-          <Header as='h4'>Date: {new Date(thought.date).toDateString()}</Header>
-          <Header as='h3'>Situation Description</Header>
+        <Modal.Content scrolling>
+          <div>
+            <Header as='h4'>Date: {new Date(thought.date).toDateString()}</Header>
+            <Header as='h3'>Situation Description</Header>
 
-          <p><strong>What happened?</strong></p>
-          <p>{this.displayField(thought.what)}</p>
+            <p><strong>What happened?</strong></p>
+            <p>{this.displayField(thought.what)}</p>
 
-          <p><strong>Where did it happen?</strong></p>
-          <p>{this.displayField(thought.where)}</p>
+            <p><strong>Where did it happen?</strong></p>
+            <p>{this.displayField(thought.where)}</p>
 
-          <p><strong>What did you expect would happen?</strong></p>
-          <p>{this.displayField(thought.expectation)}</p>
+            <p><strong>What did you expect would happen?</strong></p>
+            <p>{this.displayField(thought.expectation)}</p>
 
-          <p><strong>What actually happened?</strong></p>
-          <p>{this.displayField(thought.reality)}</p>
+            <p><strong>What actually happened?</strong></p>
+            <p>{this.displayField(thought.reality)}</p>
 
-          <Header as='h3'>Feeling Description</Header>
-          <p><strong>Initial strenght of negative feeling</strong></p>
-          <p>{this.displayField(thought.strengthInitial)}</p>
+            <Header as='h3'>Feeling Description</Header>
+            <p><strong>Initial strenght of negative feeling</strong></p>
+            <p>{this.displayField(thought.strengthInitial)}%</p>
 
-          <p><strong>Immediate negative or automatic thoughts</strong></p>
-          <p>{this.displayField(thought.automaticThought)}</p>
+            <p><strong>Immediate negative or automatic thoughts</strong></p>
+            <p>{this.displayField(thought.automaticThought)}</p>
 
-          <p>TODO: handle negative feelings generated</p>
+            <p>TODO: handle negative feelings generated</p>
 
-          <Header as='h3'>Deconstruction</Header>
-          <p><strong>Evidence that supports the negative feeling</strong></p>
-          <p>{this.displayField(thought.supportingEvidence)}</p>
+            <Header as='h3'>Deconstruction</Header>
+            <p><strong>Evidence that supports the negative feeling</strong></p>
+            <p>{this.displayField(thought.supportingEvidence)}</p>
 
-          <p><strong>Evidence that does NOT support the negative feeling</strong></p>
-          <p>{this.displayField(thought.nonSupportingEvidence)}</p>
+            <p><strong>Evidence that does NOT support the negative feeling</strong></p>
+            <p>{this.displayField(thought.nonSupportingEvidence)}</p>
 
-          <p><strong>Thinking Errors</strong></p>
-          <p>{this.displayField(thought.thinkingErrors)}</p>
+            <p><strong>Thinking Errors</strong></p>
+            <ul>{this.displayThinkingErrors(thought.thinkingErrors)}</ul>
 
-          <p><strong>New belief after reflection</strong></p>
-          <p>{this.displayField(thought.newBelief)}</p>
+            <p><strong>New belief after reflection</strong></p>
+            <p>{this.displayField(thought.newBelief)}</p>
 
-          <p><strong>New strength of emotion</strong></p>
-          <p>{this.displayField(thought.strengthFinal)}</p>
-
+            <p><strong>New strength of emotion</strong></p>
+            <p>{this.displayField(thought.strengthFinal)}%</p>
+          </div>
         </Modal.Content>
       </Modal>
     )
